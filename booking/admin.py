2179397744e2128ade0ec7e93e8d85b0695e2149
Friendly Gorillas <nataanthoni@gmail.com>
2023-11-09 20:13:12 +0300

    Ensure different models have stand a lone

--- booking/admin.py
+++ booking/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from .models import Booking, BookingType, BookingStatus
-from  client.models import Client
+from tour.models import TourOverview
+from supplier.models import Choice
 # Register your models here.
 admin.site.register(Booking)
 admin.site.register(BookingType)
 admin.site.register(BookingStatus)
-admin.site.register(Client)
--- tour/models.py
+++ tour/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from supplier.models import Supplier
 # Create your models here.
 CATEGORY_CHOICES = (
     ('NA', 'National Newspaper'),
@@ -17,43 +17,11 @@ TYPE_CHOICES = (
 )
 # Create your models here.
 
-class Operator(models.Model):
-    name = models.CharField(max_length=200)
-    logo = models.CharField(max_length=200)
-    category = models.CharField(choices=CATEGORY_CHOICES, max_length=2, default="NA")
-    o_type = models.CharField(choices=TYPE_CHOICES, max_length=2, default="FR")
-    country = models.CharField(max_length=200)
-    city = models.CharField(max_length=200, blank=True, null=True)
-    phone = models.CharField(max_length=12, blank=True, null=True)
-    phone2 = models.CharField(max_length=12, blank=True, null=True)
-    email = models.CharField(max_length=100, blank=True, null=True)
-    password = models.CharField(max_length=200, default="password0")
-    confirm_password = models.CharField(max_length=200, default="password0")
-    website = models.CharField(max_length=200, blank=True, null=True)
-    no_of_newspapers = models.CharField(max_length=200)
-    added_date = models.DateTimeField('date published')
-
-    def __str__(self):
-        return self.name
-    
-    def was_published_recently(self):
-        return self.added_date >= timezone.now() - datetime.timedelta(days=1)
-
-
-class Choice(models.Model):
-    operator = models.ForeignKey(Operator, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=100)
-    votes = models.IntegerField(default=0)
-    
-    def __str__(self):
-        return self.choice_text
-
-
 class TourOverview (models.Model):
     tour_title = models.CharField(max_length=200)
     logo = models.CharField(max_length=200)
     price = models.FloatField()
-    operator = models.ForeignKey(Operator, on_delete=models.CASCADE)
+    operator = models.ForeignKey(Supplier, on_delete=models.CASCADE)
     category = models.CharField(choices=CATEGORY_CHOICES, max_length=4, default="NA")
     n_type = models.CharField(choices=TYPE_CHOICES, max_length=4, default="FR")
     city = models.CharField(max_length=200)
